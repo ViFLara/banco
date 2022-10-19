04b627fadf524313052c9c7f2f7974663cbf0e66
Viviane Lara <viviflara@gmail.com>
2022-10-18 21:18:34 -0300

    Initial commit

---
+++ src/main/java/com/grupo4/api/banco/BancoApplication.java
@@ -0,0 +1,13 @@
+package com.grupo4.api.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo4/api/banco/BancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.api.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

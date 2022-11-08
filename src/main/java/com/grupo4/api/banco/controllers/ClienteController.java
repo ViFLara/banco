46384d043508eae450424cec36c3c1034ac83131
Viviane Lara <viviflara@gmail.com>
2022-11-08 20:00:26 -0300

    criação controller e service de cliente

--- src/main/java/com/grupo4/api/banco/controllers/ClienteController.java
+++ src/main/java/com/grupo4/api/banco/controllers/ClienteController.java
@@ -1,11 +1,41 @@
 package com.grupo4.api.banco.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.grupo4.api.banco.entities.Cliente;
+import com.grupo4.api.banco.services.ClienteService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
+
+    ClienteService service;
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Cliente findById(Long id) {
+        return service.findById(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Find all Clientes")
+    public List<Cliente> findAll() {
+        return service.findAll();
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ApiOperation("Update Cliente")
+    public void update(Cliente cliente) {
+        service.update(cliente); }
+
 
     //get
     //post
---
+++ src/main/java/com/grupo4/api/banco/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.grupo4.api.banco.services;
+
+import com.grupo4.api.banco.entities.Cliente;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class ClienteService {
+
+    //@Transactional(readOnly = true)
+    public Cliente findById(Long id) {
+        return new Cliente();
+    }
+
+    //@Transactional(readOnly = true)
+    public List<Cliente> findAll() {
+        return null;
+    }
+
+    public void update(Cliente cliente){
+    }
+}
